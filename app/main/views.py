90a6f74e9ec074d10654fa3899d67493cd53c014
Risper Akinyi <email@example.com>
2019-07-30 20:19:54 +0300

    create a variable title and pass it into our templates.

--- app/main/views.py
+++ app/main/views.py
@@ -14,7 +14,7 @@ def index():
 	sports_sources = get_sources('sports')
 	technology_sources = get_sources('technology')
 	entertainment_sources = get_sources('entertainment')
-	title = "Hapa Kule News"
+	title = "Hapa"
 
 	return render_template('index.html',title = title, sources = sources,sports_sources = sports_sources,technology_sources = technology_sources,entertainment_sources = entertainment_sources)
 
