4fa6005a5f85e3e0fc502b38330b3b0b70fa3902
Risper Akinyi <email@example.com>
2019-07-30 20:00:05 +0300

    Create a virtual environment inside our directory and name it virtual and activate it

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/moringa/Downloads/News-Highlight-master/virtual"
+set -gx VIRTUAL_ENV "/home/moringa/Desktop/projects/NewsHighlight/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
