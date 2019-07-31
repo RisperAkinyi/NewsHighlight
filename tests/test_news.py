bdf036006f4615a0b5b5d49fe3840333a7f2173a
Risper Akinyi <email@example.com>
2019-07-31 10:32:45 +0300

    add test content

--- tests/test_news.py
+++ tests/test_news.py
@@ -10,7 +10,7 @@ class SourcesTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_source = Sources('CNN','CNN News','Cable News Newtork that is a leader in providing news worldwide','cnn.com','general','U.S.A','eng')
+        self.new_source = Sources('Daily Nation','Nation news','New finance system gives school heads nightmares','dailynation.co.ke','general','Kenya','eng')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_source,Sources))
