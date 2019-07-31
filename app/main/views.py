74c4ad6f838aebecc8f7f5c3b5456f6b50e77e9f
Risper Akinyi <email@example.com>
2019-07-31 10:22:49 +0300

    change title

--- app/main/views.py
+++ app/main/views.py
@@ -12,7 +12,7 @@ def index():
 	sports_sources = get_sources('sports')
 	technology_sources = get_sources('technology')
 	entertainment_sources = get_sources('entertainment')
-	title = "Hapa Kule News"
+	title = "News"
 
 	return render_template('index.html',title = title, sources = sources,sports_sources = sports_sources,technology_sources = technology_sources,entertainment_sources = entertainment_sources)
 
